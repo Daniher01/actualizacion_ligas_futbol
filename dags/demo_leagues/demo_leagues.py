f250bed02b82137da469fda91d27cd8fe8545f0c
Daniel Hernandez <daniher02@gmail.com>
2025-06-12 17:07:56 +0200

    Actualizar el flujo de trabajo del DAG para la extracción de datos de ligas de fútbol, añadiendo tareas de validación y agrupación por liga.

--- dags/demo_leagues/demo_leagues.py
+++ dags/demo_leagues/demo_leagues.py
@@ -15,6 +15,7 @@ import airflow
 from airflow.models import Variable
 from airflow import models
 from airflow.models import DAG
+from airflow.utils.task_group import TaskGroup
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
@@ -36,6 +37,35 @@ default_arguments = {   'owner': 'Dhernandez',
                         'retry_delay':timedelta(minutes=5)}
 
 
+params_info = Variable.get("feature_info", deserialize_json=True)
+df = pd.read_csv('/usr/local/airflow/df_ligas.csv')
+df_team = pd.read_csv('/usr/local/airflow/team_table.csv')
+
+def extract_info(df ,df_team ,**kwargs):
+
+    df_data = data_processing(df) # buscar procesar por liga individual
+
+    df_final = pd.merge(df_data,df_team,how='inner',on='EQUIPO')
+    df_final = df_final[['ID_TEAM','EQUIPO', 'J', 'G', 'E', 'P', 'GF', 'GC', 'DIF', 'PTS', 'LIGA',
+        'CREATED_AT']]
+
+    df_final.to_csv('./premier_positions.csv',index=False)
+    
+def extract_info_by_liga(df_team,liga, url, **kwargs):
+    
+    print(f"🔄 PROCESANDO LIGA: {liga}")
+    print(f"🌐 URL: {url}")
+    print(f"📅 Timestamp: {datetime.now()}")
+
+    df_data = get_data(url,liga) # buscar procesar por liga individual
+
+    df_final = pd.merge(df_data,df_team,how='inner',on='EQUIPO')
+    df_final = df_final[['ID_TEAM','EQUIPO', 'J', 'G', 'E', 'P', 'GF', 'GC', 'DIF', 'PTS', 'LIGA',
+        'CREATED_AT']]
+
+    df_final.to_csv(f'./premier_positions.csv',index=False)
+
+
 with DAG('FOOTBAL_LEAGUES',
          default_args=default_arguments,
          description='Extracting Data Footbal League' ,
@@ -43,27 +73,34 @@ with DAG('FOOTBAL_LEAGUES',
          schedule  = None, # en formato cron, None si no se quiere programar
          tags=['tabla_espn'],
          catchup=False) as dag :
+    
+         grupos = []
+    
+         for index, row in df.iterrows(): 
+            liga = row['LIGA']
+            url = row['URL']
 
+            with TaskGroup(group_id=f'liga_{liga.lower()}') as grupo:
 
-         params_info = Variable.get("feature_info", deserialize_json=True)
-         df = pd.read_csv('/usr/local/airflow/df_ligas.csv')
-         df_team = pd.read_csv('/usr/local/airflow/team_table.csv')
+                extract_task = PythonOperator(
+                    task_id=f'extract_{liga.lower()}',
+                    python_callable=extract_info_by_liga,
+                    op_kwargs={"df_team": df_team, "liga": liga, "url": url}
+                )
+                
+                validate_task = PythonOperator(
+                    task_id='validate_data',
+                    python_callable=lambda liga=liga: print(f"Validando {liga}")
+                )
+                
+                extract_task >> validate_task  # Secuencia dentro del grupo
 
-         def extract_info(df ,df_team ,**kwargs):
+            grupos.append(grupo)
 
-            df_data = data_processing(df)
-
-            df_final = pd.merge(df_data,df_team,how='inner',on='EQUIPO')
-            df_final = df_final[['ID_TEAM','EQUIPO', 'J', 'G', 'E', 'P', 'GF', 'GC', 'DIF', 'PTS', 'LIGA',
-                'CREATED_AT']]
-
-            df_final.to_csv('./premier_positions.csv',index=False)
-
-
-         extract_data = PythonOperator(task_id='EXTRACT_FOTBALL_DATA',
-                                    # provide_context=True,
-                                    python_callable=extract_info,
-                                    op_kwargs={"df":df,"df_team":df_team})
+        #  extract_data = PythonOperator(task_id='EXTRACT_FOTBALL_DATA',
+        #                             # provide_context=True,
+        #                             python_callable=extract_info,
+        #                             op_kwargs={"df":df,"df_team":df_team})
 
          upload_stage = SQLExecuteQueryOperator(
 
@@ -80,4 +117,4 @@ with DAG('FOOTBAL_LEAGUES',
                     params=params_info
                     )
 
-         extract_data >>  upload_stage >> ingest_table
+         grupos >>  upload_stage >> ingest_table
